37c8ce0d196105927899e951f399899777682551
Ruben Pachas <benjaminpac22@gmail.com>
2024-10-17 09:43:12 -0500

    Plantilla AdminLTE

---
+++ app/config/App.php
@@ -0,0 +1,11 @@
+<?php
+
+//Constantes globales
+const SERVERURL = "http://localhost/Permisos/";
+const COMPANY = "SENATI";
+const VERSION = "1.0";
+
+//Configuracion horarira
+date_default_timezone_set("America/Lima");
+
+//recursos - HELPERS...
