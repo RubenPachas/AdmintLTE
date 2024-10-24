7dd397c2c057e61db2c19b9f07c2d46c2c538af5
Ruben Pachas <benjaminpac22@gmail.com>
2024-10-24 10:35:41 -0500

    Modelo Usuario y metodos login agregado

--- app/database/SPU-usuarios.sql
+++ app/database/SPU-usuarios.sql
@@ -1,2 +1,22 @@
 USE dbasistencia;
-CREATE PROCEDURE 
+
+-- MECANISMO / FORMAS / TIPOS
+-- Instructivo SQL
+-- Store Procedure
+-- ORM
+
+DROP PROCEDURE IF EXISTS `spu_usuarios_login`;
+DELIMITER //
+CREATE PROCEDURE spu_usuarios_login(IN _nomuser VARCHAR(20))
+BEGIN
+	SELECT 
+		US.idusuario,
+        PE.apellidos, PE.nombres,
+        PR.perfil, PR.nombrecorto,
+        Us.nomuser, US.passuser
+		FROM usuarios US
+        INNER JOIN personas PE ON PE.idpersona = US.idpersona
+        INNER JOIN perfiles PR ON PR.idperfil = US.idperfil
+        WHERE nomuser = _nomuser AND US.inactive_at IS NULL;
+END //
+
